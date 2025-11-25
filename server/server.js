c0377799d3ae3ae0ec9caf45f98a9de24fab1e03
laurgreg28 <laurgreg28@gmail.com>
2025-11-25 07:31:19 -0600

    updating server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const bodyParser = require('body-parser');
 
 // Initialize DB connection (uses ../src/models via mongoose models already created)
